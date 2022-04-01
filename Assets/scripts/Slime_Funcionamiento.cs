10f4d4b9bd4b3258ade5f4ea6ee3418bbda7bc1e
ismacastt <102297544+ismacastt@users.noreply.github.com>
2022-04-02 00:15:24 +0200

    +

--- Assets/scripts/Slime_Funcionamiento.cs
+++ Assets/scripts/Slime_Funcionamiento.cs
@@ -2,57 +2,103 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Slime_Funcionamiento : MonoBehaviour{
+public class Slime_Funcionamiento : MonoBehaviour
+{
 
     public Animator animator;
     public float velocidad;
-   public float vida;
-    public float danioa;
-    public float rango;
+    public float velocidadatras;
+    public float vida;
+    public float datack;
+    public float distanciaplayer;
+    public float vision;
+    public float visionatras;
     public float veloatack;
     public float rangoatack;
     public Rigidbody2D rb2d;
     public LayerMask capa;
     public Transform player;
 
-    
+
 
 
     // Start is called before the first frame update
-    void Start(){
+    void Start()
+    {
 
 
-        
+
     }
 
     // Update is called once per frame
-    void Update(){
+    void Update()
+    {
+        distanciaplayer = Vector2.Distance(player.position, rb2d.position);
+
+        if (distanciaplayer < vision && distanciaplayer > visionatras && distanciaplayer > rangoatack)
+        {
+
+            Vector2 objetivo = new Vector2(player.position.x, player.position.y);
+            Vector2 nuevaPos = Vector2.MoveTowards(rb2d.position, objetivo, velocidad * Time.deltaTime);
+
+            rb2d.MovePosition(nuevaPos);
 
 
-        Vector2 objetivo = new Vector2(player.position.x, player.position.y);
-        Vector2 nuevaPos = Vector2.MoveTowards(rb2d.position, objetivo, velocidad * Time.deltaTime);
 
-        rb2d.MovePosition(nuevaPos);
+        }
+
+        else if (distanciaplayer < vision && distanciaplayer > visionatras && distanciaplayer <= rangoatack)
+        {
+
+            Vector2 objetivo = new Vector2(player.position.x, player.position.y);
+            Vector2 nuevaPos = Vector2.MoveTowards(rb2d.position, objetivo, 0 * Time.deltaTime);
+
+            rb2d.MovePosition(nuevaPos);
+        }
+
+        else if (distanciaplayer < visionatras)
+        {
+
+            Vector2 objetivo = new Vector2(player.position.x, player.position.y);
+            Vector2 nuevaPos = Vector2.MoveTowards(rb2d.position, objetivo, velocidadatras * Time.deltaTime);
+
+            rb2d.MovePosition(nuevaPos);
+
+        }
+
+
+    }
+
+
+
+    void movimiento()
+    {
+
+
+
+
+    }
+
+
+    void ataque()
+    {
+
+
+    }
+
+
+    private void OnDrawGizmos() {
+
+        Gizmos.color = Color.yellow;
+
+        Gizmos.DrawSphere(transform.position,rangoatack);
+        Gizmos.DrawSphere(transform.position,vision);
+        Gizmos.DrawSphere(transform.position,visionatras);
         
     }
 
 
 
-    void movimiento(){
-        
-
-
-
-    }
-
-
-    void ataque(){
-
-
-    }
-
-
-    
 
 
 }
